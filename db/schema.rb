c24fd3611734b37497c1a9f7b9ef971378eb27a8
JL <world.jadi@gmail.com>
2015-05-31 02:00:34 +0700

    change migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530174707) do
+ActiveRecord::Schema.define(version: 20150530185407) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",      limit: 50, null: false
